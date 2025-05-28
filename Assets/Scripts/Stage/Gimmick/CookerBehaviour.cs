483caacf522e5a457d27cc6cbe3aa30ef0d2b83a
goroh <65319834+goroh3rd@users.noreply.github.com>
2025-05-29 01:33:32 +0900

    オーブンの画像を実装、それに伴いステージ8を微調整

--- Assets/Scripts/Stage/Gimmick/CookerBehaviour.cs
+++ Assets/Scripts/Stage/Gimmick/CookerBehaviour.cs
@@ -1,7 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class CookerBehaviour : MonoBehaviour
 {
     public CookerType type;
     public enum CookerType { Bake, Boil, Fry }
+    [SerializeField] private float interval = 0.3f; // U“®‚ÌŠÔŠu
+    [SerializeField] private float moveDistance = 0.1f; // U“®‚Ì‹——£
+    [SerializeField] private GameObject cookerImage;
+    private void Start()
+    {
+        StartCoroutine(CockerMove(interval));
+    }
+    IEnumerator CockerMove(float interval) // ˆê’èŠÔŠu‚ÅU“®‚·‚éƒRƒ‹[ƒ`ƒ“
+    {
+        while (true)
+        {
+            cookerImage.transform.position += new Vector3(0, moveDistance, 0);
+            yield return new WaitForSeconds(interval);
+            cookerImage.transform.position -= new Vector3(0, moveDistance, 0);
+            yield return new WaitForSeconds(interval);
+        }
+    }
 }
